9c66068ac5c75986d1dda56aa083481e97b250e4
Harpa Guðrún Hreinsdóttir <harpa@expectus.is>
2022-02-20 17:59:45 +0100

    basic solution finished with mock http api calls

--- MinitwitFrontend/MinitwitFrontend/Pages/IndexBase.cs
+++ MinitwitFrontend/MinitwitFrontend/Pages/IndexBase.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
+using MinitwitFrontend.Models;
+using MinitwitFrontend.Shared;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +10,46 @@ namespace MinitwitFrontend.Pages
 {
     public class IndexBase : ComponentBase
     {
+        [Inject]
+        LoginState LoginState { get; set; }
+        [Inject]
+        private NavigationManager NavigationManager { get; set; }
+        protected userDto user;
+        protected List<TwitDto> twits { get; set; }
+
+        protected override void OnParametersSet()
+        {
+            if (!LoginState.isAuthenticated)
+            {
+                NavigationManager.NavigateTo("/public-timeline");
+            }
+            user = LoginState.loggedInUser;
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            //var response = await _httpClient.PostAsJsonAsync<userDto>("usertwits", userObject);
+
+            //if (response.IsSuccessStatusCode)
+            //{
+            //    isAuthenticated = true;
+            //}
+            //else
+            //{
+            //    isAuthenticated = false;
+
+            //}
+
+
+            //fake list remove later
+            twits = new List<TwitDto>();
+            var demoTwit1 = new TwitDto();
+            demoTwit1.Date = "20.jan 2022";
+            demoTwit1.Name = "Johan Fritze Neve";
+            demoTwit1.Twit = "It was my birthday yesterday xD";
+
+            twits.Add(demoTwit1);
+
+        }
     }
 }
---
+++ MinitwitFrontend/MinitwitFrontend/Pages/LoginBase.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Components;
+using MinitwitFrontend.Models;
+using MinitwitFrontend.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace MinitwitFrontend.Pages
+{
+    public class LoginBase : ComponentBase
+    {
+        protected UserInputModel model = new UserInputModel();
+        protected bool loading;
+        protected string errorText = "";
+
+        [Inject]
+        private NavigationManager NavigationManager { get; set; }
+        [Inject]
+        LoginState LoginState { get; set; }
+        [Inject]
+        private HttpClient _httpClient { get; set; }
+   
+        protected async void OnValidSubmit()
+        {
+            var results = await LoginState.LoginUser(model);
+         
+            StateHasChanged();
+            if (results)
+            {
+                NavigationManager.NavigateTo("/");
+            }
+            else
+            {
+                errorText = "We could not log you in";
+            }
+        }
+        protected override async Task OnInitializedAsync()
+        {
+            if (LoginState.isAuthenticated)
+            {
+                NavigationManager.NavigateTo("/");
+            }
+            LoginState.OnAuthenticationChanged += StateHasChanged;
+
+        }
+    }
+}
