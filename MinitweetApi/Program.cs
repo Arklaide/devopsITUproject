4697b5429fd5b4c6194664403135f84415c27a83
Daniel Stokholm Thomsen <61188772+DanielStokholmThomsen@users.noreply.github.com>
2022-04-05 12:44:01 +0200

    logging updated

--- MinitweetApi/Program.cs
+++ MinitweetApi/Program.cs
@@ -12,6 +12,7 @@ using Serilog.Sinks.Elasticsearch;
 using System;
 using System.Reflection;
 using Serilog.Exceptions;
+using Serilog.Formatting.Elasticsearch;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -69,9 +70,6 @@ void ConfigureLogging()
     var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
     var configuration = new ConfigurationBuilder()
         .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-        .AddJsonFile(
-            $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
-            optional: true)
         .Build();
 
     Log.Logger = new LoggerConfiguration()
@@ -91,7 +89,8 @@ ElasticsearchSinkOptions ConfigureElasticSink(IConfigurationRoot configuration, 
     return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
     {
         AutoRegisterTemplate = true,
-        IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+        AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7,
+        CustomFormatter = new ElasticsearchJsonFormatter()
     };
 }
 
